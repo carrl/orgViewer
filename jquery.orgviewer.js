51fab36aa9e29782693f834010a870271d5a4c94
carrl <carrlyea@gmail.com>
2013-09-24 14:50:51 +0800

    add jquery.orgviewer.js and jquery.orgviewer.css

---
+++ jquery.orgviewer.js
@@ -0,0 +1,117 @@
+
+;(function($) {
+    $.fn.org_viewer = function() {
+	this.addClass("org-viewer");
+
+	var my_content = this.html();
+	var org_obj = org_parser(my_content, 1);
+	// show_org_obj(org_obj);
+	new_content = "<tt>" + org_html(org_obj, 1) + "</tt>";
+
+	this.html(new_content);
+
+	this.find("div[class^=star]").bind("click", function() {
+	    // press (title) show (content)
+	    var org_next = $(this).next("div");
+
+	    if (org_next.is(":hidden")) {
+		org_next.attr("lastclick", "1");
+		org_next.parents("div[lastclick=1]").attr("lastclick", "0");
+
+		org_next.show();
+	    } else {
+		if ((org_next.attr("lastclick") == 1) && (org_next.find("div[class^=org]").length>0)) {
+		    org_next.find("div").show();
+		} else {
+		    org_next.hide();
+		    org_next.find("div[class=org-content]").hide();
+		    org_next.find("div[class=org-sub]").hide();
+		}
+		org_next.attr("lastclick", "0");
+	    }
+	});
+    };
+
+    function org_parser(content, star_cnt) {
+	// convert org-mode text to object
+
+	var org_obj = [];
+	var my_content = content;
+
+	my_content = my_content.replace(/(\r\n|\r|\n)/g, "\n"); // \r\n, \r, \n => \n
+	var star = "";
+	for (var i=0; i<star_cnt; i++) {
+	    star += "\\*";
+	}
+	var REG = eval("/^" + star+ " /m");
+
+	var my_list = my_content.split(REG);
+	
+	var star1 = star + "\\*";
+	var REG1 = eval("/^" + star1 + " /m");
+	if (my_list[0].trim() != "") {   // if first char is "*"
+	    var sub_obj = {};
+	    sub_obj["title"] = "";
+	    sub_obj["content"] = my_list[0];
+
+	    org_obj.push(sub_obj);
+
+	    my_list.shift();
+	}
+	for (var i=0; i<my_list.length; i++) {
+	    if (my_list[i].trim() != "") {
+		var sub_list = my_list[i].split(/\n/);
+		var org_title = sub_list.shift();
+		var org_content = sub_list.join("\n");
+
+		var sub_obj = {};
+		sub_obj["title"] = star.replace(/\\/g,"") + " " + org_title;
+
+		if (org_content.search(REG1) != -1) {
+		    sub_obj["content"] = org_parser(org_content, star_cnt+1);
+		} else {
+		    sub_obj["content"] = org_content;
+		}
+		org_obj.push(sub_obj);
+	    }
+	}
+
+	return org_obj;
+    }
+
+    function show_org_obj(my_org_obj) {
+	// show org-mode object (console.log)
+	for (var i=0; i<my_org_obj.length; i++) {
+	    console.log(my_org_obj[i].title);
+	    if (typeof my_org_obj[i].content === "object") {
+		show_org_obj(my_org_obj[i].content);
+	    } else {
+		console.log(my_org_obj[i].content);
+	    }
+	}
+    }
+
+    function org_html(my_org_obj, level) {
+	// convert org-mode object to HTML
+	var my_org_html = "";
+
+	for (var i=0; i<my_org_obj.length; i++) {
+	    if (my_org_obj[i].title.trim() != "") {
+		my_org_html += "<div class='star" + level + "'>" +my_org_obj[i].title.replace("\n","") + "</div>";
+	    }
+	    if (typeof my_org_obj[i].content === "object") {
+		my_org_html += "<div class='org-sub'>" +org_html(my_org_obj[i].content, level+1) + "</div>";
+	    } else {
+		console.log(my_org_obj[i].content);
+		if (my_org_obj[i].title.trim() != "") {
+		    my_org_html += "<div class='org-content'>" + my_org_obj[i].content + "</div>";
+		} else {		// no title
+		    my_org_html += "<div class='org-content-sp'>" + my_org_obj[i].content + "</div>";
+		}
+	    }
+	}
+
+	return my_org_html;
+    }
+}) (jQuery);
+
